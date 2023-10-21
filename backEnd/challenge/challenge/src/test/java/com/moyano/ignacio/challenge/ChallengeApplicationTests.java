20f4c9bed9a7c84cd62bcccd198757fd46ee3798
Ignacio.moyano <ignacio.moyano@eldars.com.ar>
2023-10-21 10:03:54 -0300

    test

---
+++ backEnd/challenge/challenge/src/test/java/com/moyano/ignacio/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyano.ignacio.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
